63b0e5a6cf615ee904da7df476207252597d5042
Lokesh Pradhan <lokeshpradhan4040@gmail.com>
2026-01-04 16:38:21 +0530

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+    require('tailwindcss-animation-delay'),
+  ],
+}
